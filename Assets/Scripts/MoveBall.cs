1c9e488ffebdef1b96a0278e304374b5ec5737fd
Horasan <zekiburakhorasan@gmail.com>
2018-03-05 01:25:21 +0300

    V.0.2

---
+++ Assets/Scripts/MoveBall.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    MoveRacket SolRacket;
+    MoveRacket SagRacket;
+
+    public GameObject LeftRacket;
+    public GameObject RightRacket;
+
+    // Use this for initialization
+    void Start ()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0)*speed;
+        SolRacket = LeftRacket.GetComponent<MoveRacket>();
+        SagRacket = RightRacket.GetComponent<MoveRacket>();
+}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+		
+	}
+
+    //Skor yapma
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "LeftWall")
+        {
+            SolRacket.Score++;
+        }
+        if (other.gameObject.name == "RightWall")
+        {
+            SagRacket.Score++;
+        }
+
+        if (other.gameObject.name == "LeftRacket")
+        {
+            //raketin üstüne çarparsa yönünü x=+1 y=hesaplanacak
+            float yMinus = transform.position.y - other.gameObject.transform.position.y;
+            //Raketin uzunluğu hesaplanır
+            float RacketLenght = other.collider.bounds.size.y;
+            //floatları var olarakda tanımlasak V.S. onun float olduğunu algılayıp ona göre davranıyor
+            var y = yMinus / RacketLenght;
+            //Bulduğumuz yön vektörü
+            Vector2 direction = new Vector2(1, y);
+            //Bu vektörü topumuzun hızına ekler
+            GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+        if (other.gameObject.name == "RightRacket")
+        {
+            //raketin üstüne çarparsa yönünü x=-1 y=hesaplanacak
+            float yMinus = transform.position.y - other.gameObject.transform.position.y;
+            //Raketin uzunluğu hesaplanır
+            float RacketLenght = other.collider.bounds.size.y;
+            //floatları var olarakda tanımlasak V.S. onun float olduğunu algılayıp ona göre davranıyor
+            var y = yMinus / RacketLenght;
+            Vector2 direction = new Vector2(-1, y);
+            //Bu vektörü topumuzun hızına ekler
+            GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/MoveRacket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour
+{
+    public float speed;
+    //Dikey eksen tanımlanacak
+    public string verticalAxis;
+    Rigidbody2D rb2;
+    public float Score;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Hız tanımlandı
+        speed = 10.0f;
+        //Oyun nesnesine ait Rigitbody2D bileşeni alınıyor ve atama yapılıyor
+        rb2 = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float y = Input.GetAxisRaw(verticalAxis);
+        rb2.velocity = new Vector2(0, y) * speed;
+	}
+}
