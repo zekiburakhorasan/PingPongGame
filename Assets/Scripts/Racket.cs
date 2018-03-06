ce57d25f32ac7c00142597aeca4f3bd43dee2c8d
Horasan <zekiburakhorasan@gmail.com>
2018-03-06 03:16:49 +0300

    V.0.5 game modes and menus added

--- Assets/Scripts/Racket.cs
+++ Assets/Scripts/Racket.cs
@@ -8,7 +8,11 @@ public class Racket : MonoBehaviour
     public Text ScoreText;
     public float MoveSpeed;
     protected float Score;
-    
+
+    private void Start()
+    {
+        MoveSpeed = 10.0f;
+    }
     public void ScoreUp() //Skor yap
     {
         Score++;
---
+++ Assets/Scripts/SceneSelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelect : MonoBehaviour
+{
+	public void GoScene (string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);  //ilgili sahneyi yükler
+	}
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
